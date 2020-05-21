8fbcd0256576367767202a11e9fbea35fbc04444
freesloth <kn901020123@gmail.com>
2020-05-22 02:02:38 +0900

    하하ㅏ하하

---
+++ my3.py
@@ -0,0 +1,30 @@
+print(__name__)
+
+
+
+
+class woman():
+    '''인간'''
+    def create( name, height, weight, iq): 
+        person = woman()
+        person.name = name
+        person.weight = weight
+        person.height = height
+        person.iq = iq
+        
+        return person
+
+    def eat( self ): 
+        self.weight += 1
+        
+        print("{}가 먹어서 {}kg이 되었다 키는{}cm 아이큐는{} ".format(self.name, self.weight,self.height, self.iq))
+        
+
+    def sleep( self ):
+        self.height += 1
+        
+        print("{}가 잠을 자서 {}CM가 되었다 몽무게는{}kg 아이큐는{}".format(self.name, self.height, self.weight, self.iq))
+    def study( self ):
+        self.iq += 10
+        
+        print("{}가 공부해서 아이큐 {}로 되었다 몸무게는{}kg 키는{}cm ".format(self.name, self.iq, self.weight, self.height))
---
+++ my4.py
@@ -0,0 +1,10 @@
+# import my1
+from my3 import woman 
+
+
+person2 = woman.create("Donghyun",170,67,120)
+person2.eat()
+person2.study()
+person2.sleep()
+
+
