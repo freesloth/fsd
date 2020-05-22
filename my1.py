db8f90b76dbe44246f421ebf06eebd39e4d7e067
freesloth <kn901020123@gmail.com>
2020-05-22 15:13:59 +0900

    5월 22일

---
+++ my1.py
@@ -0,0 +1,14 @@
+print(__name__)
+
+def add(a, b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+class BusinessCard:
+    def __init__(self, name):
+        self.name = name
+
+    def print_info(self):
+        print("--------------------")
+        print("Name: ", self.name)
---
+++ my2.py
@@ -0,0 +1,9 @@
+# import my1
+from my1 import add, BusinessCard
+print(__name__)
+
+result = add(3,4)
+print(result)
+
+a = BusinessCard("Kimsungho")
+a.print_info()
